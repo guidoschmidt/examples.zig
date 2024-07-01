a2964b3ecfdbdc251c87813eda013a4dbfbd7eff
Guido Schmidt <git@guidoschmidt.cc>
2024-07-01 10:54:45 +0200

    DOC/README.md: use cli argument to select entry

--- README.md
+++ README.md
@@ -3,22 +3,7 @@
 
 ### Bulid and run the examples
 
-To run an example use `zig build run` and type in one of the listed example source like this:
+To run an example use `zig build run -- example_name` and type in one of the listed example source like this:
 ```
-zig build run
-
->>> Found the following example sourc files:
-× greet.zig
-× arrays.zig
-× files.zig
-× http-client.zig
-× buffers.zig
-× json.zig
-× bits.zig
-× input.zig
-× cpp
-× hello.zig
-
-→ Which example should be built?
-files.zig
+zig build run -- greet
 ```
--- build.zig
+++ build.zig
@@ -20,9 +20,8 @@ pub fn build(b: *std.Build) !void {
     const args = try process.argsAlloc(allocator);
     defer process.argsFree(allocator, args);
 
-
     print("\n\n>>> Found the following example sourc files:\n", .{});
-    const src_dir = try fs.cwd().openDir("./src", .{});
+    const src_dir = try fs.cwd().openDir("./src", .{ .iterate = true });
     var it = src_dir.iterate();
     var i: u32 = 0;
     while (try it.next()) |path| : (i += 1) {
@@ -30,48 +29,25 @@ pub fn build(b: *std.Build) !void {
         print("- {d} {s}\n", .{ i, path.name });
     }
 
-    const in = std.io.getStdIn();
-    var buf = std.io.bufferedReader(in.reader());
+    const example_name = args[args.len - 1];
+    const source = try std.fmt.allocPrint(allocator, "./src/{s}.zig", .{ example_name });
+    defer allocator.free(source);
+    print("\n...Building {s} {s}\n\n", .{ example_name, source });
 
-    // Get the Reader interface from BufferedReader
-    var r = buf.reader();
+    const target = b.standardTargetOptions(.{});
+    const exe_name = fs.path.basename(source);
+    const exe = b.addExecutable(.{
+        .root_source_file = b.path(source),
+        .name = exe_name,
+        .target = target
+    });
+    b.installArtifact(exe);
 
-    print("\nSelect: ", .{});
-    // Ideally we would want to issue more than one read
-    // otherwise there is no point in buffering.
-    var msg_buf: [4096]u8 = undefined;
-    const input = try r.readUntilDelimiterOrEof(&msg_buf, '\n');
-
-    if (input) | input_txt | {
-        const selection_idx = try std.fmt.parseInt(usize, input_txt, 10);
-        print("\nSelected {d}", .{ selection_idx });
-        
-        it.reset();
-        var j: u32 = 0;
-        while (j < selection_idx) : (j += 1) {
-            _ = try it.next();
-        }
-        if (try it.next()) |entry| {
-            const source = try std.fmt.allocPrint(allocator, "./src/{s}", .{ entry.name });
-            defer allocator.free(source);
-            print("\n...Building {s} {s}", .{ entry.name, source });
-
-            const target = b.standardTargetOptions(.{});
-            const exe_name = fs.path.basename(source);
-            const exe = b.addExecutable(.{
-                .root_source_file = b.path(source),
-                .name = exe_name,
-                .target = target
-            });
-            b.installArtifact(exe);
-
-            const run_cmd = b.addRunArtifact(exe);
-            run_cmd.step.dependOn(b.getInstallStep());
-            if (b.args) |buildArgs| {
-                run_cmd.addArgs(buildArgs);
-            }
-            const run_step = b.step("run", "Run the program");
-            run_step.dependOn(&run_cmd.step);
-        }
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |buildArgs| {
+        run_cmd.addArgs(buildArgs);
     }
+    const run_step = b.step("run", "Run the program");
+    run_step.dependOn(&run_cmd.step);
 }
