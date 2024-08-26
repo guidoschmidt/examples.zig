70170569cba39912f0eea4d098f87c624445efdf
Guido Schmidt <git@guidoschmidt.cc>
2024-08-26 23:09:10 +0200

    feat(sine_wave): remove @truncate calls, change frequency over time

--- src/sine_wave.zig
+++ src/sine_wave.zig
@@ -4,7 +4,7 @@
 const std = @import("std");
 
 const SAMPLE_RATE = 44100;
-const CHANNELS = 1;
+const CHANNELS = 2;
 const HEADER_SIZE = 36;
 const SUBCHUNK1_SIZE = 16;
 const AUDIO_FORMAT = 1;
@@ -22,19 +22,21 @@ fn write_header(seconds: u32, file: std.fs.File.Writer) !void {
     try file.writeInt(u16, CHANNELS, .little);
     try file.writeInt(u32, SAMPLE_RATE, .little);
     try file.writeInt(u32, SAMPLE_RATE * CHANNELS * (BIT_DEPTH / BYTE_SIZE), .little);
-    try file.writeInt(u16, @truncate(CHANNELS * (BIT_DEPTH / BYTE_SIZE)), .little);
-    try file.writeInt(u16, @truncate(BIT_DEPTH), .little);
+    try file.writeInt(u16, CHANNELS * (BIT_DEPTH / BYTE_SIZE), .little);
+    try file.writeInt(u16, BIT_DEPTH, .little);
     try file.writeAll("data");
     try file.writeInt(u32, num_samples * CHANNELS * (BIT_DEPTH / BYTE_SIZE), .little);
 }
 
-fn sine_wave(seconds: u32, file: std.fs.File.Writer, freq: f64) !void {
+fn sine_wave(comptime seconds: u32, file: std.fs.File.Writer, base_freq: f64) !void {
     var idx: u32 = 0;
+    var freq: f64 = base_freq;
     while (idx < seconds * SAMPLE_RATE) : (idx += 1) {
         const sine: f64 = @sin(((@as(f64, @floatFromInt(idx)) * 2.0 * PI) / SAMPLE_RATE) * freq);
         const sample = ((sine + 1.0) / 2.0) * 255.0;
         const arr = [_]u8{ @intFromFloat(sample) };
         try file.writeAll(arr[0..]);
+        freq += 0.001;
     }
 }
 
