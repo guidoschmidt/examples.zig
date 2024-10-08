6e8101c39f3d4fd691ac793feb1ec5b3dff5d918
Guido Schmidt <git@guidoschmidt.cc>
2024-10-08 21:27:43 +0200

    fix: extract svg example to its own file

--- src/csv-transduce.zig
+++ src/csv-transduce.zig
@@ -38,24 +38,5 @@ pub fn mul(comptime T: type, m: T, arg: T) T {
     return arg * m;
 }
 
-pub fn wrapNoOpts(comptime _: @TypeOf([]const u8), alloc: std.mem.Allocator, outer: []const u8, inner: []const u8) []const u8 {
-    return std.fmt.allocPrint(alloc, "<{s}>{s}</{s}>", .{ outer, inner, outer }) catch return "";
-}
-
-pub fn wrap(comptime _: @TypeOf([]const u8), alloc: std.mem.Allocator, outer: []const u8, options: []const u8, inner: []const u8) []const u8 {
-    return std.fmt.allocPrint(alloc, "<{s} {s}>{s}</{s}>", .{ outer, options, inner, outer }) catch return "";
-}
-
-
 pub fn main() !void {
-    // const inner = "<circle cx=\"50\" cy=\"50\" r=\"40\" stroke=\"green\" stroke-width=\"4\" fill=\"yellow\" />";
-    // _ = inner;
-
-    const svg = f.partialGeneric([]const u8, wrap, .{ allocator, "sv", "viewBox=\"0 0 300 100\" xmlns=\"http://www.w3.org/2000/svg\""});
-    const g = f.partialGeneric([]const u8, wrapNoOpts, .{ allocator, "g" });
-    // const t = f.partial2([]const u8, wrap, "text", "fill=\"black\" x=\"0\" y=\"0\"");
-    
-    const output1 = svg(g("Test"));
-    // const output = svg(output1);
-    std.debug.print("\n{s}\n{s}", .{ output1, "" });
 }
